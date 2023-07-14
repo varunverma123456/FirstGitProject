98741c40731ab9140d65ddd3021c85d169915558
varunverma123456 <varun.verma15@gmail.com>
2023-07-14 20:59:09 +0530

    changes made and committed

--- greetings.java
+++ greetings.java
@@ -3,7 +3,7 @@ class greetings
    public static void main(String [] args)	
    {
 	   System.out.println("Welcome");
-	   
+	   System.out.println("next code");
    }
 	
 	
--- hello_world.java
+++ hello_world.java
@@ -5,7 +5,7 @@ class hello_world
 public static void main(String[] args)	
 {
 	System.out.println("Hello world");
-	
+	System.out.println("next statement");
 }
 	
 	
