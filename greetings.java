51b6192d43109b840ebb6bf3bae48b477e907ac8
varunverma123456 <varun.verma15@gmail.com>
2023-07-15 18:00:43 +0530

    code committed

--- greetings.java
+++ greetings.java
@@ -1,8 +1,10 @@
 class greetings
 {
    public static void main(String [] args)	
-   {
+   {   // this is welcome message
 	   System.out.println("Welcome");
+	   
+	   // this is next code.
 	   System.out.println("next code");
    }
 	
